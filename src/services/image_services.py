9b76d11c74a6aed68e179a2115f6d310b6b74cf0
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-07-05 15:00:33 -0300

    feat: refactor image processing service to streamline file handling and message publishing

--- src/services/image_services.py
+++ src/services/image_services.py
@@ -1,41 +1,44 @@
 from model.image_model import ImageModel
-from pathlib import Path
+from mensageria.rabbitmq_client import get_channel
 from uuid import uuid4
-from PIL import Image
+from pathlib import Path
+import json
+
 
 def get_image_folder():
-    images_dir = Path("C:/Imagens_Redimensionadas")
-    images_dir.mkdir(parents=True, exist_ok=True)
-    return images_dir
-
+    folder = Path("/mnt/imagens")
+    folder.mkdir(parents=True, exist_ok=True)
+    return folder
 
 def image_services(file, image_width: int, image_height: int):
     try:
-        images_dir = get_image_folder()
-        images_width = int(image_width)
-        images_height = int(image_height)
+        image_id = str(uuid4())
+        filename = f"{image_id}_{file.filename}"
+        folder = get_image_folder()
+        file_path = folder / filename
+        file.save(file_path)
 
-        new_filename = f"{uuid4().hex}_{file.filename}"
-        image_path = images_dir / new_filename
+        message = {
+            "imageId": image_id,
+            "imageName": filename,
+            "imageWidth": int(image_width),
+            "imageHeight": int(image_height),
+            "imagePath": str(file_path)
+        }
 
-        file.save(str(image_path))
+        print("📤 Mensagem enviada para a fila:")
+        print(json.dumps(message, indent=2))
 
-        img = Image.open(image_path)
-        resized_image = img.resize((images_width, images_height))
-        resized_image.save(image_path)
-
-        image = ImageModel(
-            imageId=uuid4().hex,
-            imageName=new_filename,
-            imageWidth=resized_image.width,
-            imageHeight=resized_image.height,
-            imageType=img.format,
-            imagePath=str(image_path),
-            imagePathFinal=str(image_path),
-            imageStatus="processed"
+        connection, channel = get_channel()
+        channel.queue_declare(queue='image_processing')
+        channel.basic_publish(
+            exchange='',
+            routing_key='image_processing',
+            body=json.dumps(message)
         )
+        connection.close()
 
-        return image.to_dict()
+        return {"message": "Image processing started successfully", "imageId": image_id}
     except Exception as e:
         print(f"Error processing image: {e}")
         return {"error": str(e)}
