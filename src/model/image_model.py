ae46808110555d955ac10fc6848bac1600474cef
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-07-05 14:59:05 -0300

    refactor: remove unused imageType parameter from ImageModel constructor

--- src/model/image_model.py
+++ src/model/image_model.py
@@ -1,10 +1,9 @@
 class ImageModel:
-    def __init__(self, imageId, imageName, imageWidth, imageHeight, imageType, imagePath, imagePathFinal, imageStatus="pending"):
+    def __init__(self, imageId, imageName, imageWidth, imageHeight, imagePath, imagePathFinal, imageStatus="pending"):
         self.imageId = imageId
         self.imageName = imageName
         self.imageWidth = imageWidth
         self.imageHeight = imageHeight
-        self.imageType = imageType
         self.imagePath = imagePath
         self.imagePathFinal = imagePathFinal
         self.imageStatus = imageStatus
@@ -15,7 +14,6 @@ class ImageModel:
             "imageName": self.imageName,
             "imageWidth": self.imageWidth,
             "imageHeight": self.imageHeight,
-            "imageType": self.imageType,
             "imagePath": self.imagePath,
             "imagePathFinal": self.imagePathFinal,
             "imageStatus": self.imageStatus
