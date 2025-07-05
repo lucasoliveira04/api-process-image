9d2d0087637012b908b7b5ea16b138a77462cd5f
= <lucasolisocialmedia@gmail.com>
2025-07-05 12:05:38 -0800

    feat: implement image upload and processing functionality

--- src/services/image_services.py
+++ src/services/image_services.py
@@ -1,30 +1,41 @@
-import uuid
-import json
-from pika import BlockingConnection, ConnectionParameters
-from models.image_model import ImageModel
-from cache.redis_cache_configuration import redis_client
-from mensageria.rabbitmq_client import publish_message
+from model.image_model import ImageModel
+from pathlib import Path
+from uuid import uuid4
+from PIL import Image
 
-def handle_image_upload(file):
-    image_id = str(uuid.uuid4())
-    filename = file.filename
-    original_path = f"/uploads/{filename}"
-    processed_path = f"/processed/{filename}"
-    file.save(original_path)
-
-    image = ImageModel(
-        id=image_id,
-        filename=filename,
-        status="pending",
-        original_path=original_path,
-        processed_path=processed_path
-    )
+def get_image_folder():
+    images_dir = Path("C:/Imagens_Redimensionadas")
+    images_dir.mkdir(parents=True, exist_ok=True)
+    return images_dir
 
 
-    redis_client.set(image_id, image.json())
+def image_services(file, image_width: int, image_height: int):
+    try:
+        images_dir = get_image_folder()
+        images_width = int(image_width)
+        images_height = int(image_height)
 
-    # enviar para fila do RabbitMQ
-    publish_message(image_id)
-    
+        new_filename = f"{uuid4().hex}_{file.filename}"
+        image_path = images_dir / new_filename
 
-    return {"image_id": image_id, "status": "pending"}
+        file.save(str(image_path))
+
+        img = Image.open(image_path)
+        resized_image = img.resize((images_width, images_height))
+        resized_image.save(image_path)
+
+        image = ImageModel(
+            imageId=uuid4().hex,
+            imageName=new_filename,
+            imageWidth=resized_image.width,
+            imageHeight=resized_image.height,
+            imageType=img.format,
+            imagePath=str(image_path),
+            imagePathFinal=str(image_path),
+            imageStatus="processed"
+        )
+
+        return image.to_dict()
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        return {"error": str(e)}
